62921ff2a5b30d38707405e5b01fb70b355d7615
Paolo Lopez <okihnnj123@gmail.com>
2026-03-27 10:08:43 -0500

    correccion de consumo de api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 console.log(BASE_URL);
 export async function getPlatos() {
-    const response = await axios.get(`${BASE_URL}/api/menu`, {
+    const response = await axios.get(`${BASE_URL}/menu`, {
         headers: {
             'ngrok-skip-browser-warning': 'true'
         }
