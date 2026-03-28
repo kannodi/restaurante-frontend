061d3b0e112d66fe2db0d5e75fa720c5a3ff1f97
Paolo Lopez <okihnnj123@gmail.com>
2026-03-27 19:56:46 -0500

    mostrarmenupage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MesasPage from './pages/MesasPage'
 import NavBar from './components/NavBar'
 import ComandasPages from './pages/ComandasPage'
 import CarritoPage from './pages/CarritoPage'
+import MenuPage from './pages/MenuPage'
 function App() {
   return (
     <div>
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <MesasPage />
       <ComandasPages />
+      <MenuPage />
     </div>
   )
 }
