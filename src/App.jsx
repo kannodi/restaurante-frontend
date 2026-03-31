ec2e45fda97e899f60c23a08f1e6d7f55763c4c1
Paolo Lopez <okihnnj123@gmail.com>
2026-03-31 08:37:08 -0500

    cambiando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import CartaPage from './pages/CartaPage'
 import MesasPage from './pages/MesasPage'
 import NavBar from './components/NavBar'
@@ -5,17 +6,23 @@ import ComandasPages from './pages/ComandasPage'
 import CarritoPage from './pages/CarritoPage'
 import MenuPage from './pages/MenuPage'
 import LoginPage from './pages/LoginPage'
+
 function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
-      <LoginPage />
-      <CartaPage />
-      <MenuPage />
-      <CarritoPage />
-      <MesasPage />
-      <ComandasPages />
-    </div>
+      <Routes>
+        {/* Ruta por defecto */}
+        <Route path="/" element={<MenuPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/carta" element={<CartaPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/carrito" element={<CarritoPage />} />
+        <Route path="/mesas" element={<MesasPage />} />
+        <Route path="/comandas" element={<ComandasPages />} />
+      </Routes>
+    </Router>
   )
 }
 
