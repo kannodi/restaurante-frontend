0cb50f850ae64ecc4d011efff8d9661941a427e9
MaricieloPeralta <peraltacielito@gmail.com>
2026-03-31 09:11:10 -0500

    jfvjsemto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,11 +7,11 @@ function NavBar({ nombreRestaurante = "Restaurante Raul" }) {
             </header>
             <nav className="flex items-center justify-between px-7 mb-5">
                 <div className=' flex gap-2'>
-                    <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/PageCarta">Carta</a></button>
-                    <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/PageMesas">Mesas</a></button>
-                    <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/PageComandas">Comandas</a></button>
+                    <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/carta">Carta</a></button>
+                    <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/mesas">Mesas</a></button>
+                    <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/comandas">Comandas</a></button>
                 </div>
-                <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 flex justify-end"><a href="/PageLogin">Login</a></button>
+                <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 flex justify-end"><a href="/login">Login</a></button>
             </nav>
         </>
     )
