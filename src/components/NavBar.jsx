900abf2ca9e49b9a692ab03fd7af4e55f4157319
Paolo Lopez <okihnnj123@gmail.com>
2026-04-02 09:43:10 -0500

    navbar carrito y logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,24 @@
 import PropTypes from "prop-types"
+import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
 function NavBar({ nombreRestaurante = "Restaurante Raul" }) {
+    const navigate = useNavigate();
+    const [sesion, setSesion] = useState(!!localStorage.getItem('token'));
+    //si hay token en el localstorage, setSesion a true
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        setSesion(!!token);
+    }, []);
+
+    //cerrar sesion eliminando en token del localstorage
+    const botonLogout = () => {
+        if (confirm('¿Estas seguro de cerrar sesión?')) {
+            localStorage.removeItem('token');
+            setSesion(false);
+            navigate('/login');
+        }
+    }
+
     return (
         <>
             <header>
@@ -10,12 +29,21 @@ function NavBar({ nombreRestaurante = "Restaurante Raul" }) {
                     <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/carta">Carta</a></button>
                     <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/mesas">Mesas</a></button>
                     <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/comandas">Comandas</a></button>
+                    <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/carrito">Carrito</a></button>
+                    <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 "><a href="/menu">Menu</a></button>
                 </div>
-                <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 flex justify-end"><a href="/login">Login</a></button>
+                <div>
+                    {sesion ? (
+                        <button onClick={botonLogout} className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 flex justify-end"><a href="/login">Logout</a></button>
+                    ) : (
+                        <button className=" bg-blue-200 text-blue-400 font-bold text-xl rounded-full px-1 py-1 w-fit m-2 flex justify-end"><a href="/login">Login</a></button>
+                    )}
+                </div>
             </nav>
         </>
-    )
+    );
 }
+
 NavBar.propTypes = {
     nombreRestaurante: PropTypes.string
 };
