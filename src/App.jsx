8ccd40627a4e2fc513ca986f4d364786fca713d2
Paolo Lopez <okihnnj123@gmail.com>
2026-04-06 11:08:29 -0500

    reorden, quite protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'//RUTA PROTEGIDA
+import { Navigate } from 'react-router-dom' //NAVEGACION
+import NavBar from './components/NavBar'
+import LoginPage from './pages/LoginPage'
+import MenuPage from './pages/MenuPage'
 import CartaPage from './pages/CartaPage'
 import MesasPage from './pages/MesasPage'
-import NavBar from './components/NavBar'
 import ComandasPages from './pages/ComandasPage'
 import CarritoPage from './pages/CarritoPage'
-import MenuPage from './pages/MenuPage'
-import LoginPage from './pages/LoginPage'
-import { Navigate } from 'react-router-dom'
-import ProtectedRoute from './components/ProtectedRoute'//
 
 
 function App() {
@@ -18,11 +18,11 @@ function App() {
         {/* Ruta por defecto */}
         <Route path="/" element={<Navigate to='/login' replace />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/carta" element={<ProtectedRoute><CartaPage /></ProtectedRoute>} />
-        <Route path="/menu" element={<ProtectedRoute><MenuPage /></ProtectedRoute>} />
-        <Route path="/carrito" element={<ProtectedRoute><CarritoPage /></ProtectedRoute>} />
-        <Route path="/mesas" element={<ProtectedRoute><MesasPage /></ProtectedRoute>} />
-        <Route path="/comandas" element={<ProtectedRoute><ComandasPages /></ProtectedRoute>} />
+        <Route path="/carta" element={<CartaPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/carrito" element={<CarritoPage />} />
+        <Route path="/mesas" element={<MesasPage />} />
+        <Route path="/comandas" element={<ComandasPages />} />
       </Routes>
     </BrowserRouter>
   )
